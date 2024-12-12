de144fc46acd6564df9c5cdf02b3d13e593b4c95
Khun Myo Myat <hanmyo551@gmail.com>
2024-12-12 22:27:29 +0700

    start to write mini game

---
+++ module-3/minigame/script.js
@@ -0,0 +1,10 @@
+// var playerName = get from user input
+var palyerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+
+// default enemy's name
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
