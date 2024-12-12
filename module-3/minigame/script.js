101733bc0761e4db15d1cc6ebbe0142e9fd2b2f2
Khun Myo Myat <hanmyo551@gmail.com>
2024-12-12 22:46:52 +0700

    start to write fight function

--- module-3/minigame/script.js
+++ module-3/minigame/script.js
@@ -8,3 +8,43 @@ var playerMoney = 10;
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+// fiht function
+var fight = function () {
+  // alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
+
+  //   ask players if they would like to fight or run
+  var promptFight = window.prompt(
+    "Would you like to fight or skip? enter FIGHT OR SKIP to choose. "
+  );
+
+  // if player chooses to fight, -- fight-
+  if (promptFight === "fight" || promptFight === "FIGHT") {
+    // remove enemy's health by subtracting the amount set in the playerAttack variable
+    enemyHealth -= playerAttack;
+    window.alert(
+      palyerName +
+        " attacked " +
+        enemyName +
+        ". " +
+        enemyName +
+        " now has " +
+        enemyHealth +
+        " health remaining."
+    );
+
+    //   check player's health
+    if (playerHealth <= 0) {
+      window.alert(palyerName + " has died!");
+    } else {
+      window.alert(palyerName + " still has " + playerHealth + " health left.");
+    }
+    //   if player chooses to skip
+  } else if (promptFight === "skip" || promptFight === "SKIP") {
+    //   confirm player wants to skip
+    window.confirm("Are you sure you'd like to quit?");
+  }
+};
+
+fight();
