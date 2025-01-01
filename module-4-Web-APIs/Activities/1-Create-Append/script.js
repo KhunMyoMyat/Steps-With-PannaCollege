e168258a36240905c1399c33d775cc3802de60bd
Khun Myo Myat <hanmyo551@gmail.com>
2025-01-01 14:44:40 +0700

    add style with settribute

--- module-4-Web-APIs/Activities/1-Create-Append/script.js
+++ module-4-Web-APIs/Activities/1-Create-Append/script.js
@@ -12,7 +12,10 @@ h1.setAttribute(
 
 var info = document.createElement("div");
 body.appendChild(info);
-info.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+info.setAttribute(
+  "style",
+  "margin:auto; width:50%; text-align:center; line-height:2rem;"
+);
 
 var img = document.createElement("img");
 info.appendChild(img);
@@ -24,32 +27,52 @@ img.setAttribute(
 var kitten = document.createElement("div");
 info.appendChild(kitten);
 kitten.textContent = "This is my kitten";
+kitten.setAttribute("style", "font-size:1.5rem;");
 
 var nameEl = document.createElement("div");
 info.appendChild(nameEl);
 nameEl.textContent = "His name is Jax.";
+nameEl.setAttribute("style", "margin-bottom:2rem;");
 
 // ----------------------------------
 
 var favorite = document.createElement("div");
 body.appendChild(favorite);
 favorite.textContent = "My favorite food are:";
+favorite.setAttribute("style", "font-size:1.5rem;");
 
 var list = document.createElement("ol");
 favorite.appendChild(list);
+list.setAttribute("style", "background:#333333; padding:2rem;");
 
 var li1 = document.createElement("li");
 list.appendChild(li1);
-li1.textContent = "Apples";
+li1.textContent = "Apples 🍎";
+li1.setAttribute(
+  "style",
+  "color:white; background:#666666; padding:5px; margin-left:1.5rem;"
+);
 
 var li2 = document.createElement("li");
 list.appendChild(li2);
-li2.textContent = "Pizza";
+li2.textContent = "Pizza 🍕";
+li2.setAttribute(
+  "style",
+  "color:white; background:#777777; padding:5px; margin-left:1.5rem;"
+);
 
 var li3 = document.createElement("li");
 list.appendChild(li3);
-li3.textContent = "Dumplings";
+li3.textContent = "Dumplings 🥟";
+li3.setAttribute(
+  "style",
+  "color:white; background:#888888; padding:5px; margin-left:1.5rem;"
+);
 
 var li4 = document.createElement("li");
 list.appendChild(li4);
-li4.textContent = "Cupcakes";
+li4.textContent = "Cupcakes 🧁";
+li4.setAttribute(
+  "style",
+  "color:white; background:#999999; padding:5px; margin-left:1.5rem;"
+);
