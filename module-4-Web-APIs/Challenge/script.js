cccf275f1a9f3edcef2863f3d19852cb527ff06f
Khun Myo Myat <hanmyo551@gmail.com>
2025-01-10 16:22:19 +0700

    very confuse for me

---
+++ module-4-Web-APIs/Challenge/script.js
@@ -0,0 +1,10 @@
+// function to start the quiz
+function startQuiz() {
+  const username = document.getElementById("username").value;
+  if (username.trim() === "") {
+    alert("Please enter your name.");
+    return;
+  }
+}
+
+startQuiz();
